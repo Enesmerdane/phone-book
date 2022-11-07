cb1010adf544433097fa54d7f7c28a42846b7d3a
enesmerdane <ensmerdane@gmail.com>
2022-11-07 19:53:52 +0300

    created context/UserContext
    created context/UserContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
+import { UserProvider } from "./context/UserContext"; 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <UserProvider>
+      
+    </UserProvider>
   );
 }
 
