680e97bc6a35ad55e1acafbf9c054698e9ae4513
enesmerdane <ensmerdane@gmail.com>
2022-11-08 00:44:47 +0300

    react router module is added
    react router module is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 
 import { UserProvider } from "./context/UserContext";
 import LoginPage from "./components/Login";
+import HomePage from "./components/Home";
+
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        Welcome to the home page
+        <button >Logout</button>
+    </div>
+    
+  )
+}
+
+export default Home
